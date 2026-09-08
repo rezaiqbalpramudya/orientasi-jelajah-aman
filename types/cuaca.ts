6621889b5e1f08a490d7a9c1f8d40376189b8df5
Reza Iqbal Pramudya <reza.iqbal.pramudya@uingusdur.ac.id>
2026-09-08 10:26:52 +0700

    feat: tambah interface DataCuaca dan WeatherCardProps

---
+++ types/cuaca.ts
@@ -0,0 +1,16 @@
+// types/cuaca.ts
+
+export interface DataCuaca {
+    kota: string;
+    suhu: number;
+    kelembapan: number;
+    catatan?: string;
+  }
+  
+  export type TingkatAQI = "BAIK" | "SEDANG" | "TIDAK_SEHAT" | "BERBAHAYA";
+  
+  export interface WeatherCardProps {
+    kota: string;
+    suhu: number;
+    tingkatAQI: TingkatAQI;
+  }
